b50bfa934d381648a65fc7219afab46c3c9681a7
piyushpawar079 <piyushpawar079@gmail.com>
2025-04-20 18:23:29 +0530

    Save

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,7 @@ import { withAuth } from "next-auth/middleware"
 
 export default withAuth(
   function middleware(req, token) {
-    // console.log(req.nextauth)
-    // console.log(req.nextauth.token)
-    // console.log(token)
+    
   },
   {
     callbacks: {
